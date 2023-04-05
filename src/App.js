d912706113ed7bb5641df34e8e0dc9e3758c79ee
RumicWorldItalia <rumicworldit@gmail.com>
2023-04-05 21:03:11 +0200

    Insert stats

--- src/App.js
+++ src/App.js
@@ -4,20 +4,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" />
+          </label>
+
+          <label>
+            <p>STR</p>
+            <input name="str" />
+          </label>
+          <label>
+            <p>DEX</p>
+            <input name="dex" />
+          </label>          
+          <label>
+            <p>INT</p>
+            <input name="int" />
+          </label>
+          <label>  
+            <p>CON</p>
+            <input name="con" />
+          </label>
+          <label>
+            <p>WIS</p>
+            <input name="wis" />
+          </label>
+          <label>
+            <p>CHA</p>
+            <input name="cha" />
+          </label>
+
+          <button type="submit">Generate</button>
+        </fieldset>
+      </form>
     </div>
   );
 }
